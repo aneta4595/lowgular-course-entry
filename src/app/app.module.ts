6fe574c45513550eaa9fa748909ea680b3e75059
Aneta Walicka <aneta4595@wp.pl>
2022-09-06 21:47:29 +0200

    employees image add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeListComponentModule } from "./ui/employee-list/employee-list.component-module";
+import { EmployeeFacesComponent } from './ui/employee-faces/employee-faces.component';
+
+
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, EmployeeFacesComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     EmployeeListComponentModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -1,7 +1,12 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { EmployeeModel } from '../../model/employee.model';
+
 
 
 @Component({
@@ -11,8 +16,10 @@ import { EmployeeModel } from '../../model/employee.model';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EmployeeListComponent {
+  title: string = 'Tekst który pojawi się w HTML';
   constructor(private _httpClient: HttpClient) {}
-  data$: Observable<EmployeeModel[] | null> = this._httpClient.get<EmployeeModel[]>( 'assets/data/employees.json');
+  data$: Observable<EmployeeModel[] | null> = this._httpClient.get<
+    EmployeeModel[]>('assets/data/employees.json');
 
 
 }
