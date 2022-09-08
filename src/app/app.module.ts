ddf43cae4d10041f7a582d3dbeb9d4384509ca96
Aneta Walicka <aneta4595@wp.pl>
2022-09-08 19:53:23 +0200

    exam - list of projects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { EmployeeListComponentModule } from './ui/employee-list/employee-list.component-module';
 import { EmployeeFacesComponent } from './ui/employee-faces/employee-faces.component';
 import { EmployeeServiceModule } from './services/employee.service-module';
+import { ProjectListComponentModule } from './ui/project-list/project-list.component-module';
+import { ProjectServiceModule } from './services/project.service-module';
+import { EmployeeService } from './services/employee.service';
+import { ProjectService } from './services/project.service';
 
 @NgModule({
   declarations: [AppComponent, EmployeeFacesComponent],
@@ -14,9 +18,11 @@ import { EmployeeServiceModule } from './services/employee.service-module';
     AppRoutingModule,
     HttpClientModule,
     EmployeeServiceModule,
+    ProjectServiceModule,
     EmployeeListComponentModule,
+    ProjectListComponentModule,
   ],
-  providers: [],
+  providers: [EmployeeService, ProjectService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -5,7 +5,6 @@ import {
 } from '@angular/core';
 import { Observable } from 'rxjs';
 import { EmployeeService } from '../../services/employee.service';
-import { EmployeeModel } from '../../model/employee.model';
 import { PersonModel } from '../../model/person.model';
 
 @Component({
