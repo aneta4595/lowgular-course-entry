f1447a71396e8694c165fb3e898be8a9ff6c00f2
Aneta Walicka <aneta4595@wp.pl>
2022-09-17 11:38:34 +0200

    delete all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeeListComponentModule } from './ui/employee-list/employee-list.component-module';
-import { EmployeeFacesComponent } from './ui/employee-faces/employee-faces.component';
-import { EmployeeServiceModule } from './services/employee.service-module';
-import { ProjectListComponentModule } from './ui/project-list/project-list.component-module';
-import { ProjectServiceModule } from './services/project.service-module';
-import { EmployeeService } from './services/employee.service';
-import { ProjectService } from './services/project.service';
-import { EmployeeFormComponentModule } from './ui/employee-form/employee-form.component-module';
 
 @NgModule({
-  declarations: [AppComponent, EmployeeFacesComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    EmployeeServiceModule,
-    ProjectServiceModule,
-    EmployeeListComponentModule,
-    ProjectListComponentModule,
-    EmployeeFormComponentModule,
-  ],
-  providers: [EmployeeService, ProjectService],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/model/project.model.ts
+++
@@ -1,8 +0,0 @@
-export interface ProjectModel {
-  readonly id: number;
-  readonly name: string;
-  readonly owner: string;
-  readonly description: string;
-  readonly email: string;
-  readonly image: string;
-}
